375283d992474de5c726b040e1945cddb191dcca
Korolenko Daria <admem75213@gmail.com>
2024-02-21 16:01:20 +0200

    init task

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import contactsRouter from './routes/contactsRouter.js';
+import mongoose, { connect } from 'mongoose';
 
+const DB_HOST = "mongodb+srv://Korolenko:l3iguC2LCPOUXNw2@cluster0.w3zl1n3.mongodb.net/db-contacts?retryWrites=true&w=majority";
 const app = express();
 
+mongoose.set("strictQuery", true);
+mongoose.connect(DB_HOST)
+    .then(() => { 
+      app.listen(8080,()=>console.log("Connect success"))
+    })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+    
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', contactsRouter);
 app.use((err, req, res, next) => {
-    const {status = 500, message = "Server error"} = err;
-    res.status(status).json({ message, })
-  })
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message, })
+});
 
-app.listen(8080, () => {
-    console.log('Server has been started on port 8080...');
-})
 export default app;
